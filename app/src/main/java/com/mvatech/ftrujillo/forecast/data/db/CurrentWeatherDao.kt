ad0b33d8c1a71e21cb91a983faddd74957338f3b
franco_trujillo <franck2340@gmail.com>
2019-02-06 12:05:04 -0500

    compiling

--- app/src/main/java/com/mvatech/ftrujillo/forecast/data/db/CurrentWeatherDao.kt
+++ app/src/main/java/com/mvatech/ftrujillo/forecast/data/db/CurrentWeatherDao.kt
@@ -9,8 +9,6 @@ import com.mvatech.ftrujillo.forecast.data.db.entity.CURRENT_WEATHER_ID
 import com.mvatech.ftrujillo.forecast.data.db.entity.CurrentWeatherEntry
 import com.mvatech.ftrujillo.forecast.data.db.unitlocalized.ImperialCurrentWeatherEntry
 import com.mvatech.ftrujillo.forecast.data.db.unitlocalized.MetricCurrentWeatherEntry
-import com.mvatech.ftrujillo.forecast.data.db.unitlocalized.UnitSpecificCurrentWeatherEntry
-import kotlinx.coroutines.selects.select
 
 @Dao
 interface CurrentWeatherDao {
--- app/src/main/java/com/mvatech/ftrujillo/forecast/data/db/entity/CurrentWeatherEntry.kt
+++ app/src/main/java/com/mvatech/ftrujillo/forecast/data/db/entity/CurrentWeatherEntry.kt
@@ -9,33 +9,32 @@ const val CURRENT_WEATHER_ID = 0
 
 @Entity(tableName = "current_weather")
 data class CurrentWeatherEntry(
-    val cloud: Int,
-    @Embedded(prefix = "condition_")
-    val condition: Condition,
-    @SerializedName("feelslike_c")
-    val feelslikeC: Double,
-    @SerializedName("feelslike_f")
-    val feelslikeF: Double,
-    val humidity: Int,
-    @SerializedName("is_day")
-    val isDay: Int,
     @SerializedName("temp_c")
     val tempC: Double,
     @SerializedName("temp_f")
     val tempF: Double,
-    val uv: Double,
+    @SerializedName("is_day")
+    val isDay: Int,
+    @Embedded(prefix = "condition_")
+    val condition: Condition,
+    @SerializedName("wind_mph")
+    val windMph: Double,
+    @SerializedName("wind_kph")
+    val windKph: Double,
+    @SerializedName("wind_dir")
+    val windDir: String,
+    @SerializedName("precip_mm")
+    val precipMm: Double,
+    @SerializedName("precip_in")
+    val precipIn: Double,
+    @SerializedName("feelslike_c")
+    val feelslikeC: Double,
+    @SerializedName("feelslike_f")
+    val feelslikeF: Double,
     @SerializedName("vis_km")
     val visKm: Double,
     @SerializedName("vis_miles")
-    val visMiles: Double,
-    @SerializedName("wind_degree")
-    val windDegree: Int,
-    @SerializedName("wind_dir")
-    val windDir: String,
-    @SerializedName("wind_kph")
-    val windKph: Double,
-    @SerializedName("wind_mph")
-    val windMph: Double
+    val visMiles: Double
 ){
     @PrimaryKey(autoGenerate = false)
     var id: Int = CURRENT_WEATHER_ID
